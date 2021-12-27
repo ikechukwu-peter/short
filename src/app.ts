d1664c9e045679f7d3ec7794608ca26fb380a6d3
Ikechukwu <peter.ikechukwu@meetkumba.com>
2021-12-27 19:01:06 +0100

    feat: responsiveness

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, { Application } from "express";
 // @ts-ignore
-import rateLimit from 'express-rate-limit'
+import rateLimit from "express-rate-limit";
 //@ts-ignore
-import helmet from "helmet"
+import helmet from "helmet";
 //@ts-ignore
-import xssclean from "xss-clean"
+import xssclean from "xss-clean";
 import compression from "compression";
 import path from "path";
 import route from "./routes/url.routes";
@@ -20,21 +20,22 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(helmet)
+app.use(helmet());
 
-app.use(xssclean)
+app.use(xssclean());
 //compress app
 app.use(compression());
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-// Apply the rate limiting middleware to API calls only 
+// Apply the rate limiting middleware to API calls only
 // Routing
-app.use(route, apiLimiter)
+app.use(apiLimiter);
+app.use(route);
 
 export default app;
