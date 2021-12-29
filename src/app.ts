a7c2670f3aac76b041880b9c9abe49b66538094a
Ikechukwu <peter.ikechukwu@meetkumba.com>
2021-12-29 17:15:46 +0100

    fix: static files

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,11 @@ const app: Application = express();
 console.log(process.cwd() + "/public");
 console.log(path.join(process.cwd(), "public"));
 
+//setting static files
+app.use(express.static(path.join(process.cwd(), "public")));
 // view engine setup
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
-app.use(express.static(path.join(process.cwd(), "public")));
-app.use(express.static(path.join(process.cwd(), "css")));
 
 //Body parser, reading data from req.body
 app.use(express.json({ limit: "10kb" }));
